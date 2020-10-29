63b7f154aa76538541a5d1c77810cd1ee0ad3482
Huynh Du Luong <huynhduluong@gmail.com>
2020-10-29 20:26:01 +0700

    Updates

---
+++ precache-manifest.bd3ab6f9338afe51f3a13f53730b8048.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9ef7cf7354172ab900b5e010031260",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "49bb82e5e4139b1d7a5c",
+    "url": "/shopping-cart/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "27a1048c59944b31e8eb",
+    "url": "/shopping-cart/static/css/main.8838ba68.chunk.css"
+  },
+  {
+    "revision": "49bb82e5e4139b1d7a5c",
+    "url": "/shopping-cart/static/js/2.02fb473f.chunk.js"
+  },
+  {
+    "revision": "b4cbc74a1d53cd16e2d14f8e29ca9a06",
+    "url": "/shopping-cart/static/js/2.02fb473f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a1048c59944b31e8eb",
+    "url": "/shopping-cart/static/js/main.fe5f6643.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  }
+]);
